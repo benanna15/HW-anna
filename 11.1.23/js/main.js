35e9833adceb6b95c43c173528a597b5bec1a7e3
Anna <benanna15@gmail.com>
2023-01-12 13:49:31 +0200

    11.1.23

--- 11.1.23/js/main.js
+++ 11.1.23/js/main.js
@@ -1,12 +1,1 @@
-const Students=[]
 
-for (let i = 0; i < 5; i++) {
-    const student=prompt("Enter your name");
-    while(student===undefined){student=prompt("Enter your name")}
-    Students.push(student);  
-} 
-Students.sort();
-//console.log (Students);
-students = Students.join(', ');
-
-alert(students)
