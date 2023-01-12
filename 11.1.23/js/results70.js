5b145f389136fe3ef6a20ec644a904d422392685
Anna <benanna15@gmail.com>
2023-01-12 12:27:35 +0200

    11.1.23

--- 11.1.23/js/results70.js
+++ 11.1.23/js/results70.js
@@ -6,31 +6,32 @@ var index=0;
 for (let i = 0; i < 50; i++) {
     var fname = prompt("Enter your name ");
     var Result = parseFloat(prompt("Enter your result"));
+        while(!Result){Result = parseFloat(prompt("Enter your result"))}
     Names.push(fname);
     Results.push(Result);
 
-    console.log(Names);
-    console.log (Results);
+   /*  console.log(Names);
+    console.log (Results); */
 }
 
-var add = 0;
+var groupA = 0;
 var groupB=0;
 
 for (let i = 0; i < Results.length; i++) {
     if(Results[i]>=70){
         list.push(Names[i]);  
-        add+=Results[i];
+        groupA+=Results[i];
         index++;
-        console.log(list + add);
+       //  console.log(list + groupA); 
     }
     else{
         
         groupB+=Results[i];
-        console.log(groupB);
+        //console.log(groupB);
     }
 } 
 
-var sumA = add /index;
+var sumA = groupA /index;
 var sumB = groupB/(50 - index);
 
 alert ("Ceux qui ont une note supérieur à 70 : " +"\n" + list.join() + "\n" +"La moyenne de tous ceux qui ont réussi : "+ sumA + "\n" +"La moyenne de ceux qui ont échoué : " + sumB) 
